a7501b7c252059cf3524746553d93c86948cf3c7
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2021-09-21 21:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -138,7 +138,7 @@ These are commands to control player.
     Command: <b>/logs</b>
 
 <b>How Can I Stream In My Group</b>
-  <i>The source code of this bot is public and can be found at <a href=https://github.com/subinps/VCPlayerBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.</i>
+  <i>The source code of this bot is public and can be found at <a href=https://t.me/MoviZenX>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.</i>
 
 """
 
